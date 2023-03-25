7d21def1c1334697a0551bb2a5680e49f72057e7
Dan Payne <dan@danpayne.info>
2023-03-25 17:38:54 -0600

    add enum color scheme

--- ScrumBuddy/Models/DailyScrum.swift
+++ ScrumBuddy/Models/DailyScrum.swift
@@ -8,11 +8,21 @@
 import Foundation
 import SwiftUI
 
-struct DailyScrum {
+struct DailyScrum: Identifiable {
+  let id: UUID
   var title: String
   var attendees: [String]
   var lengthInMinutes: Int
   var theme: Theme
+  
+  init(id: UUID = UUID(), title: String, attendees: [String], lengthInMinutes: Int, theme: Theme) {
+    self.id = id
+    self.title = title
+    self.attendees = attendees
+    self.lengthInMinutes = lengthInMinutes
+    self.theme = theme
+    
+  }
 }
 
 extension DailyScrum {
@@ -21,6 +31,7 @@ extension DailyScrum {
   [
     DailyScrum(title: "Design", attendees: ["Cathy", "Dan", "Karen", "John"], lengthInMinutes: 10, theme: .yellow),
     DailyScrum(title: "App Dev", attendees: ["Katie", "Gray", "Euna", "Luis", "Darla"], lengthInMinutes: 5, theme: .orange),
-    DailyScrum(title: "Web Dev", attendees: ["Chella", "Chris", "Christina", "Eden", "Karla", "Lindsey", "Aga", "Chad", "Jenn", "Sarah"], lengthInMinutes: 5, theme: .poppy)
+    DailyScrum(title: "Web Dev", attendees: ["Chella", "Chris", "Christina", "Eden", "Karla", "Lindsey", "Aga", "Chad", "Jenn", "Sarah"], lengthInMinutes: 5, theme: .poppy),
+    DailyScrum(title: "Fight Club", attendees: ["Katie", "Gray", "Euna", "Luis", "Darla"], lengthInMinutes: 5, theme: .oxblood),
   ]
 }
