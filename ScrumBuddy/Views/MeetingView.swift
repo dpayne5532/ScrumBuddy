87b001e4563621d5d673a374f0e29ba4f33a73a9
Dan Payne <dan@danpayne.info>
2023-03-25 19:55:47 -0600

    extract meetingFooterView

--- ScrumBuddy/Views/MeetingView.swift
+++ ScrumBuddy/Views/MeetingView.swift
@@ -8,44 +8,34 @@
 import SwiftUI
 
 struct MeetingView: View {
-  @Binding var scrum: DailyScrum
-  @StateObject var scrumTimer = ScrumTimer()
-  var body: some View {
-    
-    ZStack {
-      RoundedRectangle(cornerRadius: 16.0)
-        .fill(scrum.theme.mainColor)
-      VStack {
-        MeetingHeaderView(secondsElapsed: scrumTimer.secondsElapsed, secondsRemaining: scrumTimer.secondsRemaining, theme: scrum.theme)
-        Circle()
-          .strokeBorder(lineWidth: 24)
-        HStack {
-          Text("Speaker 1 of 3")
-          Spacer()
-          Button(action: {}) {
-            Image(systemName: "forward.fill")
-          }
-          .accessibilityLabel("Next speaker")
+    @Binding var scrum: DailyScrum
+    @StateObject var scrumTimer = ScrumTimer()
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 16.0)
+                .fill(scrum.theme.mainColor)
+            VStack {
+                MeetingHeaderView(secondsElapsed: scrumTimer.secondsElapsed, secondsRemaining: scrumTimer.secondsRemaining, theme: scrum.theme)
+                Circle()
+                    .strokeBorder(lineWidth: 24, antialiased: true)
+                MeetingFooterView(speakers: scrumTimer.speakers, skipAction: scrumTimer.skipSpeaker)
+            }
         }
-      }
-      
+        .padding()
+        .foregroundColor(scrum.theme.accentColor)
+        .onAppear {
+            scrumTimer.reset(lengthInMinutes: scrum.lengthInMinutes, attendees: scrum.attendees)
+            scrumTimer.startScrum()
+        }
+        .onDisappear {
+            scrumTimer.stopScrum()
+        }
+        .navigationBarTitleDisplayMode(.inline)
     }
-    .padding()
-    .foregroundColor(scrum.theme.accentColor)
-    .onAppear {
-      scrumTimer.reset(lengthInMinutes: scrum.lengthInMinutes, attendees: scrum.attendees)
-      scrumTimer.startScrum()
-    }
-    .onDisappear {
-      scrumTimer.stopScrum()
-    }
-    .navigationBarTitleDisplayMode(.inline)
-  }
 }
 
-struct MeetingView_Preview: PreviewProvider {
-  static var previews: some View {
-    MeetingView(scrum: .constant(DailyScrum.sampleData[0]))
-    
-  }
+struct MeetingView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetingView(scrum: .constant(DailyScrum.sampleData[0]))
+    }
 }
