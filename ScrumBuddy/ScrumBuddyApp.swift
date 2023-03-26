189c76a855b368edd6ebbe6134bb3fc951386d9c
Dan Payne <dan@danpayne.info>
2023-03-26 10:37:17 -0600

    add save function

--- ScrumBuddy/ScrumBuddyApp.swift
+++ ScrumBuddy/ScrumBuddyApp.swift
@@ -9,26 +9,29 @@ import SwiftUI
 
 @main
 struct ScrumBuddyApp: App {
-  @State private var store = ScrumStore()
+  @StateObject private var store = ScrumStore()
   
   var body: some Scene {
-    WindowGroup {
-      NavigationView {
-        ScrumsView(scrums: $store.scrums)
-      }
-      .onAppear {
-        ScrumStore.load {
-          result in
-          switch result {
-          case .failure(let error):
-            fatalError(error.localizedDescription)
-          case .success(let scrums):
-            store.scrums = scrums
+      WindowGroup {
+          NavigationView {
+              ScrumsView(scrums: $store.scrums) {
+                  ScrumStore.save(scrums: store.scrums) { result in
+                      if case .failure(let error) = result {
+                          fatalError(error.localizedDescription)
+                      }
+                  }
+              }
           }
-        }
+          .onAppear {
+              ScrumStore.load { result in
+                  switch result {
+                  case .failure(let error):
+                      fatalError(error.localizedDescription)
+                  case .success(let scrums):
+                      store.scrums = scrums
+                  }
+              }
+          }
       }
-      
-    }
   }
 }
-
