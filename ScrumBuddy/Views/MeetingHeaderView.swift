0d9dd3adf35d5355e2eb44048ab23cbb687857fb
Dan Payne <dan@danpayne.info>
2023-03-25 19:44:21 -0600

    add scrumTimer

--- ScrumBuddy/Views/MeetingHeaderView.swift
+++ ScrumBuddy/Views/MeetingHeaderView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct MeetingHeaderView: View {
   let secondsElapsed: Int
   let secondsRemaining: Int
+  let theme: Theme
   
   private var totalSeconds: Int {
     secondsElapsed + secondsRemaining
@@ -27,30 +28,33 @@ struct MeetingHeaderView: View {
   
   var body: some View {
     VStack {
-      ProgressView(value: 5, total: 15)
+      ProgressView(value: progress)
+        .progressViewStyle(ScrumProgressViewStyle(theme: theme))
       HStack {
         VStack(alignment: .leading) {
           Text("Seconds Elapsed")
             .font(.caption)
-          Label("300", systemImage: "hourglass.bottomhalf.fill")
+          Label("\(secondsElapsed)", systemImage: "hourglass.bottomhalf.fill")
         }
         Spacer()
         VStack(alignment: .trailing) {
           Text("Seconds Remaining")
             .font(.caption)
-          Label("600", systemImage: "hourglass.tophalf.fill")
+          Label("\(secondsRemaining)", systemImage: "hourglass.tophalf.fill")
+            .labelStyle(.trailingIcon)
         }
       }
-      .accessibilityElement(children: .ignore)
-      .accessibilityLabel("Time remaining")
-      .accessibilityValue("\(minutesRemaining) minutes")
     }
+    .accessibilityElement(children: .ignore)
+    .accessibilityLabel("Time remaining")
+    .accessibilityValue("\(minutesRemaining) minutes")
+    .padding([.top, .horizontal])
   }
 }
 
 struct MeetingHeaderView_Previews: PreviewProvider {
-    static var previews: some View {
-        MeetingHeaderView(secondsElapsed: 60, secondsRemaining: 180)
-        .previewLayout(.sizeThatFits)
-    }
+  static var previews: some View {
+    MeetingHeaderView(secondsElapsed: 60, secondsRemaining: 180, theme: .bubblegum)
+      .previewLayout(.sizeThatFits)
+  }
 }
